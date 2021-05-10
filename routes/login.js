66a1e71cdea25f2b5af9f16d6a61da25b6cd5e7b
hwanghyunjong <zhwhd3@naver.com>
2021-05-10 18:06:04 +0900

    helpme

--- routes/login.js
+++ routes/login.js
@@ -25,10 +25,10 @@ router.get('/',  (req, res) => {
                     var { records } = result;
                     let cooper = records
                         .filter(x => x.LastName === 'Jones')[0];
-                    console.log(cooper);
+                    //console.log(cooper);
                     
                     var LastName = cooper.LastName;
-                    console.log(LastName);
+                    //console.log(LastName);
                     
                     res.render('../views/index.ejs', {
                         LastName : LastName
@@ -47,8 +47,49 @@ router.get('/',  (req, res) => {
 router.route('/').post(function(req, res) {
 	//console.log(req.body)
     var paramName = req.body.LastName || req.body.LastName;
-	var paramEmail = req.body.id || req.query.id;
-    console.log(paramName);
+	var paramEmail = req.body.Id || req.query.Id;
+    var paramBirth = req.body.birthday || req.query.birthday;
+
+    var firstnum = req.body.firstnum || req.query.firstnum;
+    var middlenum = req.body.middlenum || req.query.middlenum;
+    var lastnum = req.body.lastnum || req.query.lastnum;
+
+    var car1 =req.body.car1 || req.query.car1;
+    var car2 =req.body.car2 || req.query.car2;
+    var car3 =req.body.car3 || req.query.car3;
+    var car4 =req.body.car4 || req.query.car4;
+    var car5 =req.body.car5 || req.query.car5;
+
+    var cars = [car1,car2,car3,car4,car5];
+
+    var check1 = req.body.agree1;
+    var check2 = req.body.agree2;
+    var check3 = req.body.agree3;
+    var check4 = req.body.agree4;
+    var check5 = req.body.agree5;
+
+    var checks = [check1, check2, check3, check4, check5];
+
+    var array = [];
+    var aa = '';
+
+    for (i=0; i<5; i++) {
+        if (cars[i] == "Y") {
+            array.push(checks[i]);
+        }
+    }
+
+    //console.log(array);
+
+    for (j=0; j<array.length; j++) {
+        if (j != array.length) {
+            aa = `${array[j]};`;
+        } else {
+            aa.concat(`${array[j]}`);
+        }
+    }
+
+    console.log(aa);
 
     try {
         conn.login(
@@ -56,14 +97,28 @@ router.route('/').post(function(req, res) {
             "ckdqnr34#$" + "6sH35SQMdzKBI8RmPaf9tsfVm",
             (err, reso) => {
                 console.log('Connected to Salesforce!!!');
-                conn.sobject("Account").create({ Name : 'My Account #2' }, function(err, ret) {
-                    if (err || !ret.success) { return console.error(err, ret); }
+                conn.sobject("Lead")
+                    .create(
+                    { 
+                        LastName : paramName,
+                        Email : paramEmail,
+                        Birthday__c : paramBirth,
+                        Phone : firstnum + middlenum + lastnum,
+                        Company	 : 'sobetec',
+                        CurrencyIsoCode : 'KRW',
+                        Status : 'Open - Not Contacted',
+                        VehicleModelOfInterest__c : ``
+                        
+                    }, 
+                    function(err, ret) {
+                        if (err || !ret.success) { return console.error(err, ret); 
+                    }
                     console.log("Created record id : " + ret.id);
                     // ...
                   });
             });
         // now you can use conn to read/write data...
-        conn.logout();
+        res.redirect('/login');
     } catch (err) {
         console.error(err);
     }
